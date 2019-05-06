b13883a815394403b838a53bf4d9e806674ed659
Charlie Roche <charlie.roche@gmail.com>
2019-05-06 09:49:40 +0200

    Simple web component

---
+++ chuck-norris.js
@@ -0,0 +1,36 @@
+import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
+
+/**
+ * `chuck-norris`
+ * An element that shows random chuck norris jokes
+ *
+ * @customElement
+ * @polymer
+ * @demo demo/index.html
+ */
+class ChuckNorris extends PolymerElement {
+  static get template() {
+    return html`<style>
+        :host {
+          display: block;
+        }
+      </style>
+      <div>[[joke]]</div>`;
+  }
+
+  static get properties() {
+    return {
+      joke: {
+        type: String,
+        value: 'Waiting for Chuck to say something funny...',
+      },
+    };
+  }
+
+
+  static get is() {
+    return 'chuck-norris'
+  }
+}
+
+window.customElements.define(ChuckNorris.is, ChuckNorris);
